b3a13039ca2dc714141c2c6d0da716b392bb13bb
S0k1l <martynuky16@gmail.com>
2025-01-20 20:05:37 +0200

    Generating JWT Tokens

--- DuberDinner.Infrastructure/DependencyInjection.cs
+++ DuberDinner.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Services;
+using DuberDinner.Infrastructure.Authentication;
+using DuberDinner.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DuberDinner.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        Microsoft.Extensions.Configuration.ConfigurationManager configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         return services;
     }
 }
---
+++ DuberDinner.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using BuberDinner.Application.Common.Interfaces.Services;
+
+namespace DuberDinner.Infrastructure.Services
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
