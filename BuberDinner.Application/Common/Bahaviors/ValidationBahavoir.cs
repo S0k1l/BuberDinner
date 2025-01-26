1c2aa05d2f642ece2b748bfe999b3a9859aa4415
S0k1l <martynuky16@gmail.com>
2025-01-26 18:08:27 +0200

    Validation Behavior | MediatR + FluentValidation

---
+++ BuberDinner.Application/Common/Bahaviors/ValidationBahavoir.cs
@@ -0,0 +1,46 @@
+﻿using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace BuberDinner.Application.Common.Bahaviors
+{
+    public class ValidationBahavoir<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+        where TResponse : IErrorOr
+    {
+        private readonly IValidator<TRequest>? _validator;
+
+        public ValidationBahavoir(IValidator<TRequest>? validator = null)
+        {
+            _validator = validator;
+        }
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
+        {
+            if (_validator is null)
+            {
+                return await next();
+            }
+
+            var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
+            if (validationResult.IsValid) 
+            {
+                return await next();
+            }
+
+            var errors = validationResult.Errors
+                .ConvertAll(validationFailure => Error.Validation(
+                    validationFailure.PropertyName,
+                    validationFailure.ErrorMessage));
+
+            return (dynamic)errors;
+        }
+    }
+}
--- BuberDinner.Application/DependencyInjection.cs
+++ BuberDinner.Application/DependencyInjection.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Common.Bahaviors;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace BuberDinner.Application;
 
@@ -7,6 +14,12 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+        services.AddScoped(
+            typeof(IPipelineBehavior<,>),
+            typeof(ValidationBahavoir<,>));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services;
     }
 }
