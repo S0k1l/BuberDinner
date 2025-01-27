56c41aa274e7b3d5f7be40c0c1d3cddf89c7e771
S0k1l <martynuky16@gmail.com>
2025-01-27 17:56:12 +0200

    Implementing AggregateRoot, Entity, ValueObject

---
+++ BuberDinner.Domain/Common/Models/ValueObject.cs
@@ -0,0 +1,41 @@
+﻿namespace BuberDinner.Domain.Common.Models
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        public abstract IEnumerable<object> GetEqualityObjects();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            var valueObject = (ValueObject)obj;
+
+            return GetEqualityObjects().SequenceEqual(valueObject.GetEqualityObjects());
+        }
+
+        public static bool operator ==(ValueObject left, ValueObject right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ValueObject left, ValueObject right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override int GetHashCode() 
+        {
+            return GetEqualityObjects()
+                .Select(x => x?.GetHashCode() ?? 0)
+                .Aggregate((x,y) => x ^ y);
+        }
+
+        public bool Equals(ValueObject? other)
+        {
+            return Equals((object?)other);
+        }
+    }
+}
